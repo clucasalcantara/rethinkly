06dbf15eac0566ab16d13180c0fc6477a481b191
mvfsilva <mvfsillva@gmail.com>
2019-03-14 13:18:07 -0300

    chore: add xo, husky, prettier  and lint-staged

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-const rethinkdb = require('rethinkdb')
-
 const connect = require('./connection')
 const retrieveData = require('./retrieve-data')
 
-export default async (dbConfig) => await connect(dbConfig)
+export default async dbConfig => connect(dbConfig)
 
-export {
-  retrieveData
-}
+export { retrieveData }
