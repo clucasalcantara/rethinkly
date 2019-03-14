a24178dbb9c831d005fc7558ba42731696fba7a3
mvfsilva <mvfsillva@gmail.com>
2019-03-14 13:15:31 -0300

    patch: improve rethinkConfig

--- src/connection.js
+++ src/connection.js
@@ -4,24 +4,20 @@
  * @params dbConfig? (Object)
  */
 const rethinkdb = require('rethinkdb')
-// isDev?
+
 const isDev = process.env._DEV_
 
-// ENV configurations
 const rethinkConfig = {
-    host: !isDev ? process.env.DB_URL : process.env.DB_URL_DEV,
-    port: !isDev ? process.env.DB_PORT : process.env.DB_PORT_DEV,
-    db: !isDev ? process.env.DB_NAME : process.env.DB_NAME_DEV,
+  host: isDev ? process.env.DB_URL_DEV : process.env.DB_URL,
+  port: isDev ? process.env.DB_PORT_DEV : process.env.DB_PORT,
+  db: isDev ? process.env.DB_NAME_DEV : process.env.DB_NAME,
 }
 
 /**
- * connect
+ * Connect
  * Get an active rethinkdb instance
- * @param {Object} dbConfig 
+ * @param {Object} dbConfig
  * @returns {Object} connection
  */
-module.exports = async (dbConfig) => rethinkdb.connect(dbConfig || rethinkConfig)
-    .then(connection => connection)
-
-
-
+module.exports = async dbConfig =>
+  rethinkdb.connect(dbConfig || rethinkConfig).then(connection => connection)
